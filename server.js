678509b5ea040ac7447f497c8d746646dcb4c8b0
Parker Jones <parker.matthewjones@gmail.com>
2021-06-23 17:34:15 -0500

    server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -36,6 +43,17 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    return animal;
+}
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -51,6 +69,12 @@ app.get('/api/animals/:id', (req, res) => {
   } else {
     res.send(404);
   }
+});
+
+app.post('/api/animals', (req, res) => {
+    req.body.id = animals.length.toString();
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
 });
 
 app.listen(PORT, () => {
